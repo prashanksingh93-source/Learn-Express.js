d4d765f300c4a159ff6e2968f838e6265cdb76de
prashanksingh93-source <prashanksingh93@gmail.com>
2026-06-16 20:21:46 +0530

    basic routing

---
+++ Query_parameter/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/about', (req, res) => {
+  let name=req.query.name
+  let  age=req.query.age
+  let location=req.query.location
+  res.send(`Hello World! ${name} your age=${age} r u from ${location}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
